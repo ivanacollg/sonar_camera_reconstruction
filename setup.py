4e7fd887651969c99e51d5a6206854bcc8edb29e
Ivana Collado <43309523+ivanacollg@users.noreply.github.com>
2025-07-29 15:57:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(packages=["sonar-camera-reconstruction"], package_dir={"": "src"})
+d = generate_distutils_setup(packages=["sonar_camera_reconstruction"], package_dir={"": "src"})
 
 setup(**d)
